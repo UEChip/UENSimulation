f4b55e4641055941d098e1e902c6b61b950d531b
JessieFeng2010 <jessie_feng2010@hotmail.com>
2015-09-22 15:03:37 +0800

    Add function code of “SimulatedDataSet”
    add the class “SimulatedData”， data read from and write to the .txt file
    stored locally.

---
+++ UENSimulation/Model/SimulatedData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UENSimulation.Model
+{
+    class SimulatedData
+    {
+        double charge_HA;//储热——充放电状态(+1/-1)
+        double duration_HA;//储热——运行时间
+        double savedH_HA;//储热——当前存储电量
+        double saveT_HA;//储热——当前存储水温度
+
+        double charge_EA;//储电——充放电状态
+        double duration_EA;//储电——运行时间
+        double savedE_EA;//储电——当前存储电量
+
+        double gear_Boiler;//补燃锅炉——补燃锅炉负荷
+        double duration_Boiler;//补燃锅炉——运行时间
+
+        double envrmtdata_lout_Heat;//光热——阳光强度参数
+        double duration_Heat;//光热——运行时间
+
+        double envrmtdata_lout_Electricity;//光伏——阳光强度参数       
+        double duration_Electricity;//光伏——运行时间
+
+        double gear_UE;//泛能机——泛能机负荷
+        double duration_UE;//泛能机——运行时间        
+
+        public double Charge_HA
+        {
+            get { return charge_HA; }
+            set { charge_HA = value; }
+        }
+
+        public double Duration_HA
+        {
+            get { return duration_HA; }
+            set { duration_HA = value; }
+        }
+
+        public double SavedH_HA
+        {
+            get { return savedH_HA; }
+            set { savedH_HA = value; }
+        }
+
+        public double SaveT_HA
+        {
+            get { return saveT_HA; }
+            set { saveT_HA = value; }
+        }
+
+        public double Charge_EA
+        {
+            get { return charge_EA; }
+            set { charge_EA = value; }
+        }
+
+        public double Duration_EA
+        {
+            get { return duration_EA; }
+            set { duration_EA = value; }
+        }
+
+        public double SavedE_EA
+        {
+            get { return savedE_EA; }
+            set { savedE_EA = value; }
+        }
+
+        public double Gear_Boiler
+        {
+            get { return gear_Boiler; }
+            set { gear_Boiler = value; }
+        }
+
+        public double Duration_Boiler
+        {
+            get { return duration_Boiler; }
+            set { duration_Boiler = value; }
+        }
+
+        public double Envrmtdata_lout_Heat
+        {
+            get { return envrmtdata_lout_Heat; }
+            set { envrmtdata_lout_Heat = value; }
+        }
+
+        public double Duration_Heat
+        {
+            get { return duration_Heat; }
+            set { duration_Heat = value; }
+        }
+
+        public double Envrmtdata_lout_Electricity
+        {
+            get { return envrmtdata_lout_Electricity; }
+            set { envrmtdata_lout_Electricity = value; }
+        }
+
+        public double Duration_Electricity
+        {
+            get { return duration_Electricity; }
+            set { duration_Electricity = value; }
+        }
+
+        public double Gear_UE
+        {
+            get { return gear_UE; }
+            set { gear_UE = value; }
+        }
+
+        public double Duration_UE
+        {
+            get { return duration_UE; }
+            set { duration_UE = value; }
+        }        
+    }
+}
--- UENSimulation/SimulatedDataSet.xaml.cs
+++ UENSimulation/SimulatedDataSet.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Reflection;
+using UENSimulation.Model;
+using UENSimulation.Utility;
 
 namespace UENSimulation
 {
@@ -19,9 +22,79 @@ namespace UENSimulation
     /// </summary>
     public partial class SimulatedDataSet : Window
     {
+        SimulatedData simulatedData = new SimulatedData();
+        Txt_Handle txt_Handle = new Txt_Handle();
+
+        string path = @"..\..\Local Storage\SimulatedData.txt";
+
         public SimulatedDataSet()
         {
             InitializeComponent();
+
+            dataReadFromSimulatedData(path);
+        }
+
+        private void dataReadFromSimulatedData(string dataFilePath)
+        {
+            string[] dataRead = txt_Handle.dataRead(dataFilePath);
+
+            charge_HA.Value = Convert.ToDecimal(dataRead[0]);
+            duration_HA.Value = Convert.ToDecimal(dataRead[1]);
+            savedH_HA.Value = Convert.ToDecimal(dataRead[2]);
+            saveT_HA.Value = Convert.ToDecimal(dataRead[3]);
+
+            charge_EA.Value = Convert.ToDecimal(dataRead[4]);
+            duration_EA.Value = Convert.ToDecimal(dataRead[5]);
+            savedE_EA.Value = Convert.ToDecimal(dataRead[6]);
+
+            gear_Boiler.Value = Convert.ToDecimal(dataRead[7]);
+            duration_Boiler.Value = Convert.ToDecimal(dataRead[8]);
+
+            envrmtdata_lout_Heat.Value = Convert.ToDecimal(dataRead[9]);
+            duration_Heat.Value = Convert.ToDecimal(dataRead[10]);
+
+            envrmtdata_lout_Electricity.Value = Convert.ToDecimal(dataRead[11]);
+            duration_Electricity.Value = Convert.ToDecimal(dataRead[12]);
+
+            gear_UE.Value = Convert.ToDecimal(dataRead[13]);
+            duration_UE.Value = Convert.ToDecimal(dataRead[14]);
+        }
+
+        private void dataWriteToSimulatedData(string dataFilePath)
+        {
+            PropertyInfo[] pis = typeof(SimulatedData).GetProperties();
+            int length = pis.Length;
+
+            string[] dataWrite = new string[length];
+
+            dataWrite[0] = charge_HA.Value.ToString();
+            dataWrite[1] = duration_HA.Value.ToString();
+            dataWrite[2] = savedH_HA.Value.ToString();
+            dataWrite[3] = saveT_HA.Value.ToString();
+
+            dataWrite[4] = charge_EA.Value.ToString();
+            dataWrite[5] = duration_EA.Value.ToString();
+            dataWrite[6] = savedE_EA.Value.ToString();
+
+            dataWrite[7] = gear_Boiler.Value.ToString();
+            dataWrite[8] = duration_Boiler.Value.ToString();
+
+            dataWrite[9] = envrmtdata_lout_Heat.Value.ToString();
+            dataWrite[10] = duration_Heat.Value.ToString();
+
+            dataWrite[11] = envrmtdata_lout_Electricity.Value.ToString();
+            dataWrite[12] = duration_Electricity.Value.ToString();
+
+            dataWrite[13] = gear_UE.Value.ToString();
+            dataWrite[14] = duration_UE.Value.ToString();
+
+            txt_Handle.dataWrite(dataFilePath, dataWrite);
+        }
+
+        private void btn_OK_Click(object sender, RoutedEventArgs e)
+        {
+            dataWriteToSimulatedData(path);
+            this.Close();
         }
     }
 }
